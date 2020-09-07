e55f9631d7a53c6074d03997544c56cd0f069575
vldln-biven'мамы.net <strifee322@gmail.com>
2020-09-07 11:47:47 +0500

    commends

---
+++ Step_01.py
@@ -0,0 +1,9 @@
+# Var string: message; # pascal, c, c++, c#, Java - static typing
+# message: = 5; -> error
+# dynamic typing: python, js, php
+
+message = 'Hello, world'
+print(message)
+
+message = 5
+print(message, type(message))
